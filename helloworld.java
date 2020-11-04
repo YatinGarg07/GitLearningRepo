d8198fb55236146b8d5bfdb05791c97a427e08bd
abc <abc@example.com>
2020-11-04 18:58:43 +0530

    updated loops

---
+++ helloworld.java
@@ -0,0 +1,7 @@
+Public HelloWorld(){
+
+static void main(String args[]){
+System.out.println("Hello duniya");
+}
+
+}
--- loop.java
+++ loop.java
@@ -1,5 +1,5 @@
 Public loop(){
 static void main(String args[]){
-for(int i=0;i<5;i++){}
+for(int i=0;i<5;i++){"no no mane pi nai hai"}
 }
 }
